2f0ed18cd572a3a8ba73073c41ec4d252813382c
Aaron C Robinson <robinson.aaron.c@gmail.com>
2020-03-21 19:52:36 +0800

    Update to 1.1

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# SelfEjectableCryptosleepCaskets [![RimWorld 1.0](https://img.shields.io/badge/RimWorld-1.0-brightgreen.svg)](http://rimworldgame.com/) 
+# SelfEjectableCryptosleepCaskets [![RimWorld 1.1](https://img.shields.io/badge/RimWorld-1.1-brightgreen.svg)](http://rimworldgame.com/) 
 
 Allows pawns to self eject themselves from cryptosleeep caskets. Can be added to existing save games and removed at anytime.
+
+**Compiled code no longer necessary but kept for historical purposes**
 
 ## Works for:
 - Cryptosleep Casket
--- Source/copyFiles.bat
+++ Source/copyFiles.bat
@@ -1,7 +1,8 @@
 @echo off
 SET "ProjectName=SelfEjectableCryptosleepCaskets"
 SET "SolutionDir=C:\Users\robin\Desktop\Games\RimWorld Modding\Source\SelfEjectableCryptosleepCaskets\Source"
+SET "RWModsDir=D:\SteamLibrary\steamapps\common\RimWorld\Mods"
 @echo on
 
-xcopy /S /Y "%SolutionDir%\..\About\*" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\%ProjectName%\About\"
-xcopy /S /Y "%SolutionDir%\..\Patches\*" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\%ProjectName%\Patches\"
+xcopy /S /Y "%SolutionDir%\..\About\*" "%RWModsDir%\%ProjectName%\About\"
+xcopy /S /Y "%SolutionDir%\..\Patches\*" "%RWModsDir%\%ProjectName%\Patches\"
